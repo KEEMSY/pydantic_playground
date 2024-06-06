758d221630cbe01936f8d9a449f436c5e633d2ef
keemsy <phzzz@naver.com>
2024-06-06 23:21:28 +0900

    Add: MC 2.StrictAndLaxTypeCoercion.py 내 JSON 관련 내용 추가 - JSON 타입에 대한 내용 추가

--- model configuration/2.StrictAndLaxTypeCoercion.py
+++ model configuration/2.StrictAndLaxTypeCoercion.py
@@ -9,6 +9,13 @@
 - model_config = ConfigDict(strict=True)를 사용하여 엄격한 유형 변환을 사용할 수 있다.
 - 관련 공식문서: https://docs.pydantic.dev/latest/concepts/conversion_table/
 
+JSON 데이터를 역직렬화 할 때는 동작이 다름에 주의해아한다.
+- boolean: true or false(소문자 주의)
+- number(float 또는 int는 실제로 중요하지 않음)
+- string: "string" (따옴표 주의, 큰 따옴표를 사용해야 한다.)
+- null: 문자열 null으로 표시되며, 따옴표를 붙이지 않는다.
+- array(배열): 대괄호, Array 또는 객체를 포함한 임의의 JSON 유형의 쉼표로 구분된 값
+- object(객체): 기본적으로 접힌 괄호(`{`)으로 구분된 dictionary. key는 문자열이어야 하며, value는 객체 자체를 포함한 모든 JSON 유형이 가능하다.
 """
 
 from pydantic import BaseModel, ValidationError, ConfigDict
