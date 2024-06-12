399e93fa27f601f560e40ddfb9033a5831ee43fa
keemsy <phzzz@naver.com>
2024-06-12 22:43:32 +0900

    Edit: MC 5. Mutablility.py 내용 수정 - Immutable 으로 설정할 경우, Dict의 key값으로 사용이 가능에 대한 내용 추가

--- model configuration/5. Mutablity.py
+++ model configuration/5. Mutablity.py
@@ -2,6 +2,7 @@
 기본적으로 Pydantic 모델은 수정가능하다. 즉, 모델 인스턴스가 생성된 뒤, 인스턴스의 값을 수정할 수 있다.
 그러나 설정을 통해 모델 인스턴스의 값을 수정할 수 없도록 할 수 있다.
 - model_config = ConfigDict(frozen=True)
+- 모델을 Immutable로 설정한다는 것은, 모델 인스턴스를 dictionary의 key 값으로 사용할 수 있다는 사이드이펙트가 존재한다.
 """
 from pydantic import BaseModel, ConfigDict
 
@@ -14,6 +15,16 @@ class MutableModel(BaseModel):
 m = MutableModel(a=1, b=2)
 m.a = 10
 print(m.dict())  # 출력: {'a': 10, 'b': 2}
+print()
+
+try:
+    d = {m: "model"}
+except TypeError as ex:
+    print(ex)
+    """
+    unhashable type: 'MutableModel'
+    """
+
 print()
 print("--------------------")
 
@@ -36,3 +47,8 @@ a
     For further information visit https://errors.pydantic.dev/2.7/v/frozen_instance
 
     """
+
+print()
+
+d = {m1: "model"}
+print(d)  # 출력: {ImmutableModel(a=1, b=2): 'model'}
